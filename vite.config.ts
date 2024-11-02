c71970d2031d8cb4378614a7d0126100341791da
Zaman Sheikh <zaman6545@gmail.com>
2024-11-03 00:48:36 +0600

    git v2.3

--- vite.config.ts
+++ vite.config.ts
@@ -32,13 +32,3 @@ export default defineConfig(({ command, mode }) => {
     },
   };
 });
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/",
-//   plugins: [react()],
-// });
