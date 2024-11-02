6bd55fd6d557dd1d41996752028ab1e35914d17d
Zaman Sheikh <zaman6545@gmail.com>
2024-11-03 00:10:01 +0600

    add cv

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/zamansheikh/",
-//   plugins: [react()],
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/zamansheikh/",
+  plugins: [react()],
+});
 
 
 // import { defineConfig } from 'vite';
@@ -20,11 +20,11 @@
 // });
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/",
+//   plugins: [react()],
+// });
