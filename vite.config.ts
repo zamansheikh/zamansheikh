efd4f376f51c218fc5a918c9579743156f797e49
Zaman Sheikh <zaman6545@gmail.com>
2024-11-03 00:02:37 +0600

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,23 @@
 // });
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command, mode }) => {
+//   return {
+//     base: mode === 'github' ? '/zamansheikh/' : '/',
+//     plugins: [react()],
+//   };
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    base: mode === 'github' ? '/zamansheikh/' : '/',
-    plugins: [react()],
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
 });
