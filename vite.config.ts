609433da7e1cfff8426b87e9d30a1558a5cb3e62
Zaman Sheikh <zaman6545@gmail.com>
2024-11-03 00:22:43 +0600

    fix directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "/zamansheikh/",
   plugins: [react()],
+  build: {
+    outDir: 'docs', // Specify the output directory here
+  },
 });
 
 
